17b2cdda6687a0f1c5341db8638c8990c31d2733
Gianluigi De Marco <dmgiangi@gmail.com>
2022-05-18 23:06:17 +0200

    updated pom with generation of jar with dependecies and class path. added README.md insert in gitignore the jar.

--- src/test/java/org/gianluigi/weather/openweather/WeatherFormatterTest.java
+++ src/test/java/org/gianluigi/weather/openweather/WeatherFormatterTest.java
@@ -1,6 +1,12 @@
 package org.gianluigi.weather.openweather;
 
+import org.apache.commons.io.IOUtils;
 import junit.framework.TestCase;
+import org.gianluigi.weather.OpenWeatherParser;
+import org.gianluigi.weather.Weather;
+import org.gianluigi.weather.WeatherFormatter;
+
+import java.io.InputStream;
 
 public class WeatherFormatterTest extends TestCase {
     public WeatherFormatterTest(String name) {
@@ -8,6 +14,10 @@ public class WeatherFormatterTest extends TestCase {
     }
 
     public void testFormatter() throws Exception {
-        //TODO implements testFormatter TEST
+        InputStream data = getClass().getClassLoader().getResourceAsStream("weather-data.xml");
+        Weather weather = new OpenWeatherParser().parse(data);
+        String formattedResult = new WeatherFormatter().format(weather);
+        InputStream expected = getClass().getClassLoader().getResourceAsStream("format-expected.dat");
+        assertEquals(IOUtils.toString(expected).trim(), formattedResult.trim());
     }
 }
